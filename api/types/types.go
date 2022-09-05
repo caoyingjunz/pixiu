e48e70cc5713b715580f4a71a5ce1eab1e7ce641
caoyingjunz <cao.yingjunz@gmail.com>
2022-09-05 22:56:53 +0800

    Refactor changePassword API

--- api/types/types.go
+++ api/types/types.go
@@ -16,17 +16,8 @@ limitations under the License.
 
 package types
 
-// TimeSpec 通用时间规格
-type TimeSpec struct {
-	GmtCreate   interface{} `json:"gmt_create,omitempty"`
-	GmtModified interface{} `json:"gmt_modified,omitempty"`
-}
-
-
-type Password struct {
-	CurrentPassword string `json:"current_password"`
-	NewPassword     string `json:"new_password"`
-	ReNewPassword   string `json:"re_new_password"`
+type IdOptions struct {
+	Id int64 `uri:"id" binding:"required"`
 }
 
 type ListOptions struct {
@@ -66,6 +57,13 @@ type User struct {
 	TimeSpec
 }
 
+type Password struct {
+	UserId          int64  `json:"user_id"`
+	OriginPassword  string `json:"origin_password"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
+}
+
 type Cloud struct {
 	Id              int64  `json:"id"`
 	ResourceVersion int64  `json:"resource_version"`
@@ -75,4 +73,10 @@ type Cloud struct {
 	Description     string `json:"description"`
 
 	TimeSpec
+}
+
+// TimeSpec 通用时间规格
+type TimeSpec struct {
+	GmtCreate   interface{} `json:"gmt_create,omitempty"`
+	GmtModified interface{} `json:"gmt_modified,omitempty"`
 }
