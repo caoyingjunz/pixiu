8f3820ba428dbc2cefbaeab9964c8723fc15b1be
caoyingjunz <cao.yingjunz@gmail.com>
2022-09-09 20:24:23 +0800

    Revert "Add demo app api completed (get, post) (#26)" (#130)

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -28,7 +28,6 @@ import (
 	"github.com/caoyingjunz/gopixiu/api/server/middleware"
 	"github.com/caoyingjunz/gopixiu/api/server/router/cicd"
 	"github.com/caoyingjunz/gopixiu/api/server/router/cloud"
-	"github.com/caoyingjunz/gopixiu/api/server/router/demo"
 	"github.com/caoyingjunz/gopixiu/api/server/router/user"
 	"github.com/caoyingjunz/gopixiu/cmd/app/options"
 	"github.com/caoyingjunz/gopixiu/pkg/pixiu"
@@ -75,10 +74,10 @@ func NewServerCommand() *cobra.Command {
 func InitRouters(opt *options.Options) {
 	middleware.InitMiddlewares(opt.GinEngine) // 注册中间件
 
-	demo.NewRouter(opt.GinEngine)  // 注册 demo 路由
 	cicd.NewRouter(opt.GinEngine)  // 注册 cicd 路由
 	cloud.NewRouter(opt.GinEngine) // 注册 cloud 路由
 	user.NewRouter(opt.GinEngine)  // 注册 user 路由
+	cicd.NewRouter(opt.GinEngine)  // 注册 cicd 路由
 }
 
 func Run(opt *options.Options) error {
