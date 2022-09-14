94b7322df879fcfedf29f40acf9b8551e9df5dde
caoyingjunz <cao.yingjunz@gmail.com>
2022-09-14 22:21:04 +0800

    Complete run graceful pixiu server

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -117,10 +117,13 @@ func runGraceServer(opt *options.Options) {
 
 	// Wait for interrupt signal to gracefully shut down the server with a timeout of 5 seconds.
 	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 	klog.Infof("shutting pixiu server down ...")
 
+	// The context is used to inform the server it has 5 seconds to finish the request
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
