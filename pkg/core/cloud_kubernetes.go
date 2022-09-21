fc861c123665dc163c25095a6442556bc7ed8cdc
程小白 <422459409@qq.com>
2022-09-21 09:34:31 +0800

    Update cloud_kubernetes.go

--- pkg/core/cloud_kubernetes.go
+++ pkg/core/cloud_kubernetes.go
@@ -47,6 +47,7 @@ func (c *cloud) Jobs(cloud string) pixiukubernetes.JobInterface {
 func (c *cloud) Ingress(cloud string) pixiukubernetes.IngressInterface {
 	return pixiukubernetes.NewIngress(clientSets.Get(cloud), cloud)
 }
+
 func (c *cloud) Nodes(cloud string) pixiukubernetes.NodeInterface {
 	return pixiukubernetes.NewNodes(clientSets.Get(cloud), cloud)
 }
