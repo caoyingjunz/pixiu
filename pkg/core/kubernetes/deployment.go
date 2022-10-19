b42d970929f0cfc3d20bfbc7c1ed075c3ae14180
程小白 <422459409@qq.com>
2022-10-19 20:06:18 +0800

    Update deployment.go

--- pkg/core/kubernetes/deployment.go
+++ pkg/core/kubernetes/deployment.go
@@ -33,9 +33,9 @@ type DeploymentsGetter interface {
 
 type DeploymentInterface interface {
 	Create(ctx context.Context, deployment *v1.Deployment) error
+	Update(ctx context.Context, deployment *v1.Deployment) error
 	Delete(ctx context.Context, deleteOptions types.GetOrDeleteOptions) error
 	List(ctx context.Context, listOptions types.ListOptions) ([]v1.Deployment, error)
-	Update(ctx context.Context, deployment *v1.Deployment) error
 }
 
 type deployments struct {
@@ -59,6 +59,20 @@ func (c *deployments) Create(ctx context.Context, deployment *v1.Deployment) err
 		Create(ctx, deployment, metav1.CreateOptions{}); err != nil {
 		log.Logger.Errorf("failed to create %s namespace %s: %v", c.cloud, deployment.Namespace, err)
 
+		return err
+	}
+
+	return nil
+}
+
+func (c *deployments) Update(ctx context.Context, deployment *v1.Deployment) error {
+	if c.client == nil {
+		return clientError
+	}
+	if _, err := c.client.AppsV1().
+		Deployments(deployment.Namespace).
+		Update(ctx, deployment, metav1.UpdateOptions{}); err != nil {
+		log.Logger.Errorf("failed to update %s deployment: %v", c.cloud, err)
 		return err
 	}
 
@@ -92,18 +106,4 @@ func (c *deployments) List(ctx context.Context, listOptions types.ListOptions) (
 	}
 
 	return deploy.Items, nil
-}
-
-func (c *deployments) Update(ctx context.Context, deployment *v1.Deployment) error {
-	if c.client == nil {
-		return clientError
-	}
-	if _, err := c.client.AppsV1().
-		Deployments(deployment.Namespace).
-		Update(ctx, deployment, metav1.UpdateOptions{}); err != nil {
-		log.Logger.Errorf("failed to update %s deployment: %v", c.cloud, err)
-		return err
-	}
-
-	return nil
 }
