48a63a4cab5c089868f5790d0acaaee7e5a764fe
caoyingjunz <cao.yingjunz@gmail.com>
2022-11-02 22:45:52 +0800

    Refacotr the Authentication middleware

--- pkg/types/constant.go
+++ pkg/types/constant.go
@@ -14,25 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package middleware
+package types
 
-import (
-	"os"
+const UserId = "userId"
 
-	"github.com/gin-gonic/gin"
-	"k8s.io/apimachinery/pkg/util/sets"
+const (
+	WebShellURL = "/clouds/webshell/ws"
 )
-
-var AlwaysAllowPath sets.String
-
-func InitMiddlewares(ginEngine *gin.Engine) {
-	// 初始化可忽略的请求路径
-	AlwaysAllowPath = sets.NewString("/healthz", "/users/login", "/users/logout")
-
-	ginEngine.Use(Cors(), LoggerToFile(), UserRateLimiter(100, 20))
-	// TODO: 临时关闭
-	if os.Getenv("DEBUG") != "true" {
-		ginEngine.Use(Authentication)
-		ginEngine.Use(Rbac())
-	}
-}
