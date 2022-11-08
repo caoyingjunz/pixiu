77b51bfe93098982bd9b721bd235c60f13a779d6
caoyingjunz <cao.yingjunz@gmail.com>
2022-11-08 21:59:50 +0800

    add

--- api/server/middleware/middleware.go
+++ api/server/middleware/middleware.go
@@ -31,6 +31,7 @@ func InitMiddlewares(ginEngine *gin.Engine) {
 	// 初始化可忽略的请求路径
 	AlwaysAllowPath = sets.NewString(types.HealthURL, types.LoginURL, types.LogoutURL)
 
+	// 依次进行跨域，日志，单用户限速，总量限速，验证，和鉴权
 	ginEngine.Use(Cors(), LoggerToFile(), UserRateLimiter(), Limiter(), Authentication())
 	// TODO: 临时关闭
 	if os.Getenv("DEBUG") != "true" {
