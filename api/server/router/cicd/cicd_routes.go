f2b1d0360e068122c9a58b16d3803b7813d22e92
caoyingjunz <cao.yingjunz@gmail.com>
2022-11-16 23:28:24 +0800

    add

--- api/server/router/cicd/cicd_routes.go
+++ api/server/router/cicd/cicd_routes.go
@@ -26,15 +26,6 @@ import (
 	"github.com/caoyingjunz/gopixiu/pkg/pixiu"
 )
 
-// runJob godoc
-// @Summary      runJob jenkins job
-// @Description  runJob jenkins job
-// @Tags         runJob
-// @Accept       json
-// @Produce      json
-// @Success      200  {object}  httputils.Response
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/run [post]
 func (s *cicdRouter) runJob(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -52,16 +43,6 @@ func (s *cicdRouter) runJob(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// createJob godoc
-// @Summary      create jenkins job
-// @Description  create jenkins job
-// @Tags         create
-// @Accept       json
-// @Produce      json
-// @Param        data body types.Cicd true "git"
-// @Success      200  {object}  httputils.Response
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/createJob [post]
 func (s *cicdRouter) createJob(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -78,16 +59,6 @@ func (s *cicdRouter) createJob(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// copyJob godoc
-// @Summary      copy jenkins job
-// @Description  copy jenkins job
-// @Tags         copy
-// @Accept       json
-// @Produce      json
-// @Param        data body types.Cicd true "oldName, newName"
-// @Success      200  {object}  httputils.Response
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/copy [post]
 func (s *cicdRouter) copyJob(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -102,16 +73,6 @@ func (s *cicdRouter) copyJob(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// renameJob godoc
-// @Summary      rename jenkins job
-// @Description  rename jenkins job
-// @Tags         rename
-// @Accept       json
-// @Produce      json
-// @Param        data body types.Cicd true "oldName, newName"
-// @Success      200  {object}  httputils.Response
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/rename [post]
 func (s *cicdRouter) renameJob(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -126,15 +87,6 @@ func (s *cicdRouter) renameJob(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// getAllJobs godoc
-// @Summary      get jenkins allJob
-// @Description  get jenkins allJob
-// @Tags         getAllJobs
-// @Accept       json
-// @Produce      json
-// @Success      200  {object}  httputils.Response
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs [get]
 func (s *cicdRouter) getAllJobs(c *gin.Context) {
 	r := httputils.NewResponse()
 	var err error
@@ -146,15 +98,6 @@ func (s *cicdRouter) getAllJobs(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// getAllViews godoc
-// @Summary      get jenkins allView
-// @Description  get jenkins allView
-// @Tags         getAllViews
-// @Accept       json
-// @Produce      json
-// @Success      200  {object}  httputils.Response{result=[]string}
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/view [get]
 func (s *cicdRouter) getAllViews(c *gin.Context) {
 	r := httputils.NewResponse()
 	var err error
@@ -166,16 +109,6 @@ func (s *cicdRouter) getAllViews(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// getLastFailedBuild godoc
-// @Summary      get jenkins LastFailedBuild
-// @Description  get jenkins LastFailedBuild
-// @Tags         getLastFailedBuild
-// @Accept       json
-// @Produce      json
-// @Param        data body types.Cicd true "name"
-// @Success      200  {object}  httputils.Response{result=[]string}
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/failed [post]
 func (s *cicdRouter) getLastFailedBuild(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -192,16 +125,6 @@ func (s *cicdRouter) getLastFailedBuild(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// getLastFailedBuild godoc
-// @Summary      get jenkins LastFailedBuild
-// @Description  get jenkins LastFailedBuild
-// @Tags         getLastFailedBuild
-// @Accept       json
-// @Produce      json
-// @Param        data body types.Cicd true "name"
-// @Success      200  {object}  httputils.Response{result=[]string}
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/failed [post]
 func (s *cicdRouter) getLastSuccessfulBuild(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -236,16 +159,6 @@ func (s *cicdRouter) getAllNodes(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// deleteJob godoc
-// @Summary      delete jenkins job
-// @Description  delete jenkins job
-// @Tags         deleteJob
-// @Accept       json
-// @Produce      json
-// @Param        name
-// @Success      200  {object}  httputils.Response{result=[]string}
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/failed [post]
 func (s *cicdRouter) deleteJob(c *gin.Context) {
 	r := httputils.NewResponse()
 	name := c.Param("name")
@@ -295,15 +208,6 @@ func (s *cicdRouter) addViewJob(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// restart godoc
-// @Summary      restart jenkins server
-// @Description  restart jenkins server
-// @Tags         restart
-// @Accept       json
-// @Produce      json
-// @Success      200  {object}  httputils.Response
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/restart [post]
 func (s *cicdRouter) restart(c *gin.Context) {
 	r := httputils.NewResponse()
 	if err := pixiu.CoreV1.Cicd().Restart(context.TODO()); err != nil {
@@ -313,16 +217,6 @@ func (s *cicdRouter) restart(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// disable godoc
-// @Summary      disable jenkins job
-// @Description  disable jenkins job
-// @Tags         disable
-// @Accept       json
-// @Produce      json
-// @Param        cicd.Name  path string  true  "name"  Format(string)
-// @Success      200
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/disable [post]
 func (s *cicdRouter) disable(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -337,16 +231,6 @@ func (s *cicdRouter) disable(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// enable godoc
-// @Summary      enable jenkins job
-// @Description  enable jenkins job
-// @Tags         enable
-// @Accept       json
-// @Produce      json
-// @Param        cicd.Name  path string  true  "name"  Format(string)
-// @Success      200
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/enable [post]
 func (s *cicdRouter) enable(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -361,16 +245,6 @@ func (s *cicdRouter) enable(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// stop godoc
-// @Summary      stop jenkins job
-// @Description  stop jenkins job
-// @Tags         stop
-// @Accept       json
-// @Produce      json
-// @Param        cicd.Name  path string  true  "name"  Format(string)
-// @Success      200
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/stop [post]
 func (s *cicdRouter) stop(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -385,16 +259,6 @@ func (s *cicdRouter) stop(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// config godoc
-// @Summary      config jenkins job
-// @Description  config jenkins job
-// @Tags         config
-// @Accept       json
-// @Produce      json
-// @Param        cicd.Name  path string  true  "name"  Format(string)
-// @Success      200  {object}  httputils.Response{result=config}
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/config [post]
 func (s *cicdRouter) config(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
@@ -425,16 +289,6 @@ func (s *cicdRouter) updateConfig(c *gin.Context) {
 	httputils.SetSuccess(c, r)
 }
 
-// history godoc
-// @Summary      history jenkins job
-// @Description  history jenkins job
-// @Tags         history
-// @Accept       json
-// @Produce      json
-// @Param        cicd.Name  path string  true  "name"  Format(string)
-// @Success      200  {object}  httputils.Response{result=[]*gojenkins.History}
-// @Failure      400  {object}  httputils.Response
-// @Router       /cicd/jobs/history [post]
 func (s *cicdRouter) history(c *gin.Context) {
 	r := httputils.NewResponse()
 	var cicd types.Cicd
