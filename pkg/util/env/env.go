65a411e6ee6e38a5cf80927810a1e791cb9bdf90
caoyingjunz <cao.yingjunz@gmail.com>
2022-11-17 22:47:48 +0800

    add env utils for debug mode

---
+++ pkg/util/env/env.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2021 The Pixiu Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package env
+
+import (
+	"os"
+	"strings"
+)
+
+func EnableDebug() bool {
+	debug := os.Getenv("DEBUG")
+	if len(debug) == 0 {
+		return false
+	}
+
+	return strings.ToLower(debug) == "true"
+}
