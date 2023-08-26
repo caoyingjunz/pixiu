176b071757e1e7be1f1863ab773d5b3a147ae165
caoyingjunz <cao.yingjunz@gmail.com>
2023-08-26 18:58:42 +0800

    add

--- pkg/db/factory.go
+++ pkg/db/factory.go
@@ -23,28 +23,16 @@ import (
 )
 
 type ShareDaoFactory interface {
-<<<<<<< HEAD
 	Cloud() cloud.Interface
 	KubeConfig() cloud.Interface
-	User() user.Interface
-=======
-	Cloud() cloud.CloudInterface
-	KubeConfig() cloud.KubeConfigInterface
->>>>>>> 9b81bdc42738ee1721f1f0336bbe081fc9ad7414
 }
 
 type shareDaoFactory struct {
 	db *gorm.DB
 }
 
-<<<<<<< HEAD
 func (f *shareDaoFactory) Cloud() cloud.Interface      { return cloud.NewCloud(f.db) }
 func (f *shareDaoFactory) KubeConfig() cloud.Interface { return cloud.NewKubeConfig(f.db) }
-func (f *shareDaoFactory) User() user.Interface        { return user.NewUser(f.db) }
-=======
-func (f *shareDaoFactory) Cloud() cloud.CloudInterface           { return cloud.NewCloud(f.db) }
-func (f *shareDaoFactory) KubeConfig() cloud.KubeConfigInterface { return cloud.NewKubeConfig(f.db) }
->>>>>>> 9b81bdc42738ee1721f1f0336bbe081fc9ad7414
 
 func NewDaoFactory(db *gorm.DB) ShareDaoFactory {
 	return &shareDaoFactory{
