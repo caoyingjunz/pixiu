33dc8673e3546480be5f790e7e4d8fcaeb591432
caoyingjunz <cao.yingjunz@gmail.com>
2023-09-09 16:43:32 +0800

    Revert "Optimise the signal and remove unused utils (#329)"
    This reverts commit ac62fe76ee5cae2aaa455897f8b3f3b6bf7a164c.

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -17,16 +17,19 @@ limitations under the License.
 package app
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/spf13/cobra"
 	"k8s.io/klog/v2"
 
 	"github.com/caoyingjunz/pixiu/api/server/router"
 	"github.com/caoyingjunz/pixiu/cmd/app/options"
-	"github.com/caoyingjunz/pixiu/pkg/signals"
 )
 
 func NewServerCommand() *cobra.Command {
@@ -85,8 +88,16 @@ func Run(opt *options.Options) error {
 		}
 	}()
 
-	// Set up signals so we handle the shutdown signal gracefully
-	ctx := signals.SetupSignalHandler()
+	// Wait for interrupt signal to gracefully shut down the server with a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	klog.Infof("shutting pixiu server down ...")
+
+	// The context is used to inform the server it has 5 seconds to finish the request
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	if err := srv.Shutdown(ctx); err != nil {
 		klog.Fatal("pixiu server forced to shutdown: ", err)
 	}
