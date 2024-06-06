4aaf357f3127b2ff174f3ec027ed8fb9b02f2d0a
Gitlayzer <18037803502@163.com>
2024-06-06 10:51:17 +0800

    Add deployment plan related (#409)
    Co-authored-by: gitlayzer <gitlayzer@devops-engineer.com.cn>

---
+++ pkg/controller/plan/deploy.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2021 The Pixiu Authors.
+
+Licensed under the Apache License, Version 2.0 (phe "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plan
+
+import (
+	"context"
+	"time"
+
+	"k8s.io/klog/v2"
+
+	"github.com/caoyingjunz/pixiu/pkg/util/container"
+)
+
+type Deploy struct {
+	handlerTask
+}
+
+func (b Deploy) Name() string { return "部署集群" }
+
+// Run 以容器的形式执行 BootStrap 任务，如果存在旧的容器，则先删除在执行
+func (b Deploy) Run() error {
+	klog.Infof("starting 部署集群 task")
+	defer klog.Infof("completed 部署集群 task")
+
+	cli, err := container.NewContainer("deploy", b.GetPlanId())
+	if err != nil {
+		return err
+	}
+	defer cli.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
+	defer cancel()
+
+	// 启动执行容器
+	if err = cli.StartAndWaitForContainer(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
