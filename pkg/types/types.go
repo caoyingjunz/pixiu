48c42938b323ad78b44d2a4ed132bfc500d3e897
caoyingjunz <cao.yingjunz@gmail.com>
2024-06-23 11:35:55 +0800

    修复任务状态 (#438)

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -107,9 +107,9 @@ type Plan struct {
 	PixiuMeta `json:",inline"`
 	TimeMeta  `json:",inline"`
 
-	Name        string         `json:"name"` // 用户名称
-	Step        model.PlanStep `json:"step"`
-	Description string         `json:"description"` // 用户描述信息
+	Name        string           `json:"name"` // 用户名称
+	Step        model.TaskStatus `json:"step"`
+	Description string           `json:"description"` // 用户描述信息
 
 	Config PlanConfig `json:"config"`
 	Nodes  []PlanNode `json:"nodes"`
