4a7d2ab8a621f05b908b5ac9b01b70329f2bc03c
jihu <jihu@dtstack.com>
2024-07-28 11:08:01 +0800

    feat(pixiu):alert audit event for middleware

--- api/server/middleware/audit.go
+++ api/server/middleware/audit.go
@@ -3,8 +3,10 @@ package middleware
 import (
 	"context"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/caoyingjunz/pixiu/api/server/httputils"
 	"github.com/caoyingjunz/pixiu/cmd/app/options"
@@ -39,21 +41,82 @@ func saveAudit(o *options.Options, c *gin.Context, obj, sid string) {
 	var userName string
 	ip := c.ClientIP()
 	user := c.Value("user")
-	//debug阶段user不存在
 	if _, ok := user.(model.User); !ok {
 		userName = "unknown"
 	} else {
 		userName = user.(model.User).Name
 	}
-	object := &model.Audit{
-		Action:   c.Request.Method,
-		Content:  buildContent(obj, c.Request.Method, c.Request.RequestURI),
-		Ip:       ip,
-		Operator: userName,
+	switch obj {
+	case User:
+		user, err := o.Factory.User().Get(context.TODO(), StringToInt64(sid))
+		if err != nil {
+			user = &model.User{}
+		}
+		object := &model.Audit{
+			Action:   c.Request.Method,
+			Content:  buildContent(obj, c.Request.Method, c.Request.RequestURI, user),
+			Ip:       ip,
+			Operator: userName,
+		}
+		o.Factory.Audit().Create(context.TODO(), object)
+	case Cluster:
+		cluster, err := o.Factory.Cluster().Get(context.TODO(), StringToInt64(sid))
+		if err != nil {
+			cluster = &model.Cluster{}
+		}
+		object := &model.Audit{
+			Action:   c.Request.Method,
+			Content:  buildContent(obj, c.Request.Method, c.Request.RequestURI, cluster),
+			Ip:       ip,
+			Operator: userName,
+		}
+		o.Factory.Audit().Create(context.TODO(), object)
+	case Plan:
+		plan, err := o.Factory.Plan().Get(context.TODO(), StringToInt64(sid))
+		if err != nil {
+			plan = &model.Plan{}
+		}
+		object := &model.Audit{
+			Action:   c.Request.Method,
+			Content:  buildContent(obj, c.Request.Method, c.Request.RequestURI, plan),
+			Ip:       ip,
+			Operator: userName,
+		}
+		o.Factory.Audit().Create(context.TODO(), object)
+	case Tenant:
+		tenant, err := o.Factory.Tenant().Get(context.TODO(), StringToInt64(sid))
+		if err != nil {
+			tenant = &model.Tenant{}
+		}
+		object := &model.Audit{
+			Action:   c.Request.Method,
+			Content:  buildContent(obj, c.Request.Method, c.Request.RequestURI, tenant),
+			Ip:       ip,
+			Operator: userName,
+		}
+		o.Factory.Audit().Create(context.TODO(), object)
 	}
-	o.Factory.Audit().Create(context.TODO(), object)
 }
 
-func buildContent(obj, method, url string) string {
-	return fmt.Sprintf("操作资源[%s];操作请求[%s]:[%s]", obj, method, url)
+func buildContent(obj, method, url string, objModel interface{}) string {
+	switch objModel.(type) {
+	case *model.User:
+		return fmt.Sprintf("操作资源[%s];操作请求[%s]:[%s];被操作对象[%s]", obj, method, url, objModel.(*model.User).Name)
+	case *model.Cluster:
+		return fmt.Sprintf("操作资源[%s];操作请求[%s]:[%s];被操作对象[%s]", obj, method, url, objModel.(*model.Cluster).Name)
+	case *model.Plan:
+		return fmt.Sprintf("操作资源[%s];操作请求[%s]:[%s];被操作对象[%s]", obj, method, url, objModel.(*model.Plan).Name)
+	case *model.Tenant:
+		return fmt.Sprintf("操作资源[%s];操作请求[%s]:[%s];被操作对象[%s]", obj, method, url, objModel.(*model.Tenant).Name)
+	default:
+		return fmt.Sprintf("操作资源[%s];操作请求[%s]:[%s];被操作对象[%s]", obj, method, url, "新增model请添加审计")
+	}
+}
+
+func StringToInt64(s string) int64 {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0
+	}
+	return i
 }
