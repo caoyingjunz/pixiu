0f725d75628dcab615f8351913239e01abffe8ef
caoyingjunz <cao.yingjunz@gmail.com>
2024-08-12 23:31:36 +0800

    Add deploymente page list supported (#497)

--- pkg/controller/cluster/informer.go
+++ pkg/controller/cluster/informer.go
@@ -21,9 +21,10 @@ import (
 	"fmt"
 	"sort"
 
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	appsv1 "k8s.io/client-go/listers/apps/v1"
+	listersv1 "k8s.io/client-go/listers/apps/v1"
 	v1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/klog/v2"
 
@@ -65,7 +66,7 @@ func (c *cluster) GetPod(ctx context.Context, podsLister v1.PodLister, namespace
 	return pod, nil
 }
 
-func (c *cluster) GetDeployment(ctx context.Context, deploymentsLister appsv1.DeploymentLister, namespace string, name string) (interface{}, error) {
+func (c *cluster) GetDeployment(ctx context.Context, deploymentsLister listersv1.DeploymentLister, namespace string, name string) (interface{}, error) {
 	deploy, err := deploymentsLister.Deployments(namespace).Get(name)
 	if err != nil {
 		klog.Error("failed to get deployment (%s/%s) from indexer: %v", namespace, name, err)
@@ -140,11 +141,52 @@ func (c *cluster) podsForPage(pods []*corev1.Pod, pageOption types.PageRequest) 
 	return pods[offset:end]
 }
 
-func (c *cluster) ListDeployments(ctx context.Context, deploymentsLister appsv1.DeploymentLister, namespace string, pageOption types.PageRequest) (interface{}, error) {
+// ListDeployments
+// TODO: 后续优化
+func (c *cluster) ListDeployments(ctx context.Context, deploymentsLister listersv1.DeploymentLister, namespace string, pageOption types.PageRequest) (interface{}, error) {
 	deployments, err := deploymentsLister.Deployments(namespace).List(labels.Everything())
 	if err != nil {
 		return nil, err
 	}
 
-	return deployments, nil
+	sort.Slice(deployments, func(i, j int) bool {
+		// sort by creation timestamp in descending order
+		if deployments[j].ObjectMeta.GetCreationTimestamp().Time.Before(deployments[i].ObjectMeta.GetCreationTimestamp().Time) {
+			return true
+		} else if deployments[i].ObjectMeta.GetCreationTimestamp().Time.Before(deployments[j].ObjectMeta.GetCreationTimestamp().Time) {
+			return false
+		}
+
+		// if the creation timestamps are equal, sort by name in ascending order
+		return deployments[i].ObjectMeta.GetName() < deployments[j].ObjectMeta.GetName()
+	})
+	if len(namespace) == 0 {
+		sort.Slice(deployments, func(i, j int) bool {
+			return deployments[i].ObjectMeta.GetNamespace() < deployments[j].ObjectMeta.GetNamespace()
+		})
+	}
+
+	return types.PageResponse{
+		PageRequest: pageOption,
+		Total:       len(deployments),
+		Items:       c.deploymentsForPage(deployments, pageOption),
+	}, nil
+}
+
+func (c *cluster) deploymentsForPage(deployments []*appsv1.Deployment, pageOption types.PageRequest) interface{} {
+	if !pageOption.IsPaged() {
+		return deployments
+	}
+
+	total := len(deployments)
+	offset := (pageOption.Page - 1) * pageOption.Limit
+	if offset > total {
+		return nil
+	}
+	end := offset + pageOption.Limit
+	if end > total {
+		end = total
+	}
+
+	return deployments[offset:end]
 }
