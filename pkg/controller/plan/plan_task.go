bed71299e5ae3625fdadfc4ebcdf48bc20e3baa5
lei <934366858@qq.com>
2024-12-03 20:07:44 +0800

    修复读取部署任务日志异常问题

--- pkg/controller/plan/plan_task.go
+++ pkg/controller/plan/plan_task.go
@@ -139,8 +139,7 @@ func (p *plan) WatchTaskLog(ctx context.Context, planId int64, taskId int64, w h
 	scanner := bufio.NewScanner(readCloser)
 	flush, _ := w.(http.Flusher)
 	for scanner.Scan() {
-		line := scanner.Bytes()
-		line = append(line, byte('\n'))
+		line := append(scanner.Bytes(), byte('\n'))
 		// 去掉前8不可见字符
 		_, err = w.Write(line[8:])
 		if err == io.EOF {
