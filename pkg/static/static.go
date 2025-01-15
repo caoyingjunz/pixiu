03a090f4e7ea2fa08a9a4bcd9d7bc1364029fc47
caoyingjunz <cao.yingjunz@gmail.com>
2025-01-15 22:00:09 +0800

    add

--- pkg/static/static.go
+++ pkg/static/static.go
@@ -1,31 +1,30 @@
 /*
 Copyright 2024 The Pixiu Authors.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package static
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type ServeFileSystem interface {
 	http.FileSystem
 	Exists(prefix string, path string) bool
+}
+
+func ServeRoot(urlPrefix, root string) gin.HandlerFunc {
+	return Serve(urlPrefix, LocalFile(root, false))
 }
 
 // Serve returns a middleware handler that serves static files in the given directory.
