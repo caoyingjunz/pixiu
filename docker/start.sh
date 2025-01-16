9e93c7822a6592cf4d00ec01ed0975d59d23108c
caoyingjunz <cao.yingjunz@gmail.com>
2025-01-17 01:07:46 +0800

    简化镜像启动依赖配置 (#571)

--- docker/start.sh
+++ docker/start.sh
@@ -9,8 +9,15 @@ fi
 if [[ -e "/static/config.json" ]]; then
     rm -rf /static/config.json
 fi
-if [[ -e "/etc/pixiu/config.json" ]]; then
-    cp /etc/pixiu/config.json /static/config.json
+
+URL=$(grep "url" /etc/pixiu/config.yaml | awk -F'url:' '{print $2}' | tr -d '[:space:]')
+if [[  -z "$URL" ]]; then
+    URL="http://localhost:8080"
 fi
+cat >/static/config.json << EOF
+{
+    "url": "${URL}"
+}
+EOF
 
 /app
