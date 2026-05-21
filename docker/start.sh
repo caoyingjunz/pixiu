9875d11ef5d045dfc2dcf150bc6f8d37bbe04326
caoyingjunz <cao.yingjunz@gmail.com>
2026-05-21 17:02:26 +0800

    优化 pixiu 部署镜像构建流程 (#636)

--- docker/start.sh
+++ docker/start.sh
@@ -6,18 +6,8 @@ if [[ ! -d "/etc/pixiu" ]]; then
     mkdir -p /etc/pixiu
 fi
 
-if [[ -e "/static/config.json" ]]; then
-    rm -rf /static/config.json
-fi
+# 暂时支持 80 端口
+cp /nginx-default-80.conf /etc/nginx/nginx.conf
 
-URL=$(grep "url" /etc/pixiu/config.yaml | awk -F'url:' '{print $2}' | tr -d '[:space:]')
-if [[  -z "$URL" ]]; then
-    URL="http://localhost:8080"
-fi
-cat >/static/config.json << EOF
-{
-    "url": "${URL}"
-}
-EOF
-
-/app
+/app &
+nginx -g "daemon off;"
