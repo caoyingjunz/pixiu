9633cb6d5e97f538c0fe4dd2f85a4b966c0fa168
caoyingjunz <cao.yingjunz@gmail.com>
2026-06-15 15:42:19 +0800

    支持 kubeconfig 的点击下载 (#691)

--- pkg/controller/cluster/permission.go
+++ pkg/controller/cluster/permission.go
@@ -414,9 +414,8 @@ func (c *cluster) permissionModel2Type(o *model.Permission) *types.Permission {
 		Rules:             decodeRules(o.Rules),
 		SAName:            o.SAName,
 		SANamespace:       o.SANamespace,
-		ClusterId:         o.OwnerClusterId,
-		ClusterName:       o.OwnerClusterName,
-		ClusterAliasName:  o.OwnerClusterAliasName,
+		ClusterId:         o.ClusterId,   // 对应生成的k8s集群ID
+		ClusterName:       o.ClusterName, // 集群名称
 		TargetNamespaces:  decodeStringSlice(o.TargetNamespaces),
 		Description:       o.Description,
 	}
