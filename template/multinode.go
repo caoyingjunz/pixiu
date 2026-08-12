579eb09797c1a3227a7fa3a6415aa5cf819d9e0b
puzhihao <pzh905655347@gmail.com>
2026-08-12 22:38:06 +0800

    支持sudo部署

--- template/multinode.go
+++ template/multinode.go
@@ -20,7 +20,7 @@ const MultiModeTemplate = `# Render below by Pixiu engine
 [docker-master]
 {{- range .DockerMaster }}
 {{- if eq .Auth.Type "password" }}
-{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}
+{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}{{ if ne .Auth.Password.User "root" }} ansible_become=true ansible_become_method=sudo ansible_become_user=root ansible_become_password={{ .Auth.Password.Password }}{{ end }}
 {{- end }}
 {{- if eq .Auth.Type "key" }}
 {{ .Name }} ansible_ssh_user=root ansible_ssh_private_key_file={{ .Auth.Key.File }}
@@ -30,7 +30,7 @@ const MultiModeTemplate = `# Render below by Pixiu engine
 [docker-node]
 {{- range .DockerNode }}
 {{- if eq .Auth.Type "password" }}
-{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}
+{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}{{ if ne .Auth.Password.User "root" }} ansible_become=true ansible_become_method=sudo ansible_become_user=root ansible_become_password={{ .Auth.Password.Password }}{{ end }}
 {{- end }}
 {{- if eq .Auth.Type "key" }}
 {{ .Name }} ansible_ssh_user=root ansible_ssh_private_key_file={{ .Auth.Key.File }}
@@ -40,7 +40,7 @@ const MultiModeTemplate = `# Render below by Pixiu engine
 [containerd-master]
 {{- range .ContainerdMaster }}
 {{- if eq .Auth.Type "password" }}
-{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}
+{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}{{ if ne .Auth.Password.User "root" }} ansible_become=true ansible_become_method=sudo ansible_become_user=root ansible_become_password={{ .Auth.Password.Password }}{{ end }}
 {{- end }}
 {{- if eq .Auth.Type "key" }}
 {{ .Name }} ansible_ssh_user=root ansible_ssh_private_key_file={{ .Auth.Key.File }}
@@ -50,7 +50,7 @@ const MultiModeTemplate = `# Render below by Pixiu engine
 [containerd-node]
 {{- range .ContainerdNode }}
 {{- if eq .Auth.Type "password" }}
-{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}
+{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}{{ if ne .Auth.Password.User "root" }} ansible_become=true ansible_become_method=sudo ansible_become_user=root ansible_become_password={{ .Auth.Password.Password }}{{ end }}
 {{- end }}
 {{- if eq .Auth.Type "key" }}
 {{ .Name }} ansible_ssh_user=root ansible_ssh_private_key_file={{ .Auth.Key.File }}
@@ -60,7 +60,7 @@ const MultiModeTemplate = `# Render below by Pixiu engine
 [storage]
 {{- range .StorageNode }}
 {{- if eq .Auth.Type "password" }}
-{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}
+{{ .Name }} ansible_ssh_user={{ .Auth.Password.User }} ansible_ssh_pass={{ .Auth.Password.Password }}{{ if ne .Auth.Password.User "root" }} ansible_become=true ansible_become_method=sudo ansible_become_user=root ansible_become_password={{ .Auth.Password.Password }}{{ end }}
 {{- end }}
 {{- if eq .Auth.Type "key" }}
 {{ .Name }} ansible_ssh_user=root ansible_ssh_private_key_file={{ .Auth.Key.File }}
